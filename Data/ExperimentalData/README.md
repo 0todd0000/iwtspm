e4d4e6e0e1b838879f8d6ccf2eb59e8f24f1fc1a
Todd Pataky <todd@h74.50.242.10.103784.vlan.kuins.net>
2020-11-30 16:21:37 +0900

    Add all experimental data results

--- Data/ExperimentalData/README.md
+++ Data/ExperimentalData/README.md
@@ -1,8 +1,9 @@
 # Experimental Datasets
 
-The following experimental datasets are redistributed under the terms of their original licenses:
+The following experimental datasets were processed, and processed data are redistributed under the terms of the original licenses:
 
-* Besier2009
+* Besier2009a
+* Besier2009b
 * Dorn2012
 * Kautz1991a
 * Kautz1991b
@@ -10,13 +11,25 @@ The following experimental datasets are redistributed under the terms of their o
 
 ___
 
-## Besier 2009
+## Besier 2009a
 
 * **Group 0**: Controls (healthy)
 * **Group 1**: Patellofemoral pain
-* **Dependent variable**:  Estimated medial gastrocnemius muscl force (N)
+* **Dependent variable**:  Estimated semimembranosus muscle force (N)
 * **Reference**:  Besier TF, Fredericson M, Gold GE, Beaupré GS, Delp SL. Knee muscle forces during walking and running in patellofemoral pain patients and pain-free controls. Journal of biomechanics. 2009 May 11;42(7):898-905. DOI: [10.1016/j.jbiomech.2009.01.032](https://dx.doi.org/10.1016/j.jbiomech.2009.01.032)
 * **Original dataset**:  [https://simtk.org/home/muscleforces](https://simtk.org/home/muscleforces)
+* **License**: SimTK, No license (see [https://simtk.org/faq.php](https://simtk.org/faq.php))
+
+
+___
+
+## Besier 2009b
+
+{same as Besier 2009a execept:}
+
+
+* **Dependent variable**:  Estimated medial gastrocnemius muscle force (N)
+
 
 
 ___
@@ -28,6 +41,7 @@ ___
 * **Dependent variable**:  Anterior ground reaction force (N)
 * **Reference**: Dorn TW, Schache AG, Pandy MG. Muscular strategy shift in human running: dependence of running speed on hip and ankle muscle performance. Journal of Experimental Biology. 2012 Jun 1;215(11):1944-56. DOI: [10.1242/jeb.064527](https://dx.doi.org/10.1242/jeb.064527)
 * **Original dataset**:  [https://simtk.org/home/runningspeeds](https://simtk.org/home/runningspeeds)
+* **License**: MIT
 
 ___
 
@@ -38,7 +52,7 @@ ___
 * **Dependent variable**:  Normal pedal force (N)
 * **Reference**:  Kautz SA, Feltner ME, Coyle EF, Baylor AM. The pedaling technique of elite endurance cyclists: changes with increasing workload at constant cadence. Journal of Applied Biomechanics. 1991 Feb 1;7(1):29-53. DOI: [10.1123/ijsb.7.1.29](https://doi.org/10.1123/ijsb.7.1.29)
 * **Original dataset**:  [http://isbweb.org/data/kautz/](http://isbweb.org/data/kautz/)
-
+* **License**: ISB, No license (see [ISB Data Resources](http://isbweb.org/data/))
 
 ___
 
@@ -59,5 +73,5 @@ ___
 * **Dependent variable**:  Knee flexion (deg)
 * **Reference**: Neptune RR, Wright IC, Van Den Bogert AJ. Muscle coordination and function during cutting movements. Medicine and science in sports and exercise. 1999 Feb 1;31(2):294-302. DOI: [10.1097/00005768-199902000-00014](https://doi.org/10.1097/00005768-199902000-00014)
 * **Original dataset**:  [http://isbweb.org/data/rrn/index.html](http://isbweb.org/data/rrn/index.html)
-
+* **License**: ISB, No license (see [ISB Data Resources](http://isbweb.org/data/))
 
---
+++ Python/ExperimentalData/all_analyses.py
@@ -0,0 +1,131 @@
+
+'''
+Analyze all experimental datasets using three techniques:
+1. IWT
+2. SPM
+3. SnPM   (permutation tests, using t_max)
+
+
+Experimental data
+'''
+
+
+
+import os,unipath
+import numpy as np
+from scipy import stats
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+import spm1d
+
+
+plt.style.use('bmh')
+mpl.rcParams['xtick.labelsize'] = 'small'
+mpl.rcParams['ytick.labelsize'] = 'small'
+mpl.rcParams['font.sans-serif'] = 'Arial'
+
+
+def load_csv(fnameCSV):
+	a            = np.loadtxt(fname_data, delimiter=',')
+	group        = a[:,0]
+	y            = a[:,1:]
+	u            = np.unique(group)
+	y0,y1        = y[group==u[0]], y[group==u[1]]
+	return y0,y1
+
+
+def calc_p_unadjusted(y0, y1):
+	spm          = spm1d.stats.ttest2(y0, y1, equal_var=False)
+	z,df         = spm.z, spm.df[1]
+	p            = stats.f.sf(z**2, 1, df)
+	return p
+
+def calc_p_adjusted_spm(y0, y1):
+	spm          = spm1d.stats.ttest2(y0, y1, equal_var=False)
+	z,df         = spm.z, spm.df[1]
+	p            = spm1d.rft1d.f.sf(z**2, (1,df), spm.Q, spm.fwhm, withBonf=True)
+	return p
+
+
+def calc_p_adjusted_snpm(y0, y1, niter=1000):
+	n0,n1      = y0.shape[0], y1.shape[0]
+	group      = [0]*n0 + [1]*n1
+	y          = np.vstack([y0, y1])
+	snpm       = spm1d.stats.nonparam.anova1(y, group)
+	niter      = -1 if (niter > snpm.nPermUnique) else niter
+	snpmi      = snpm.inference(iterations=niter)
+	pdf        = np.abs( snpmi.PDF0 )
+	p          = np.array([ (pdf > z).mean() for z in snpmi.z])
+	return p
+
+def calc_p_adjusted_iwt(fname_data, seed=1, niter=1000):
+	fname_Rscript     = os.path.join( dirREPO, 'R', 'run_iwt_two_tailed.R')
+	fname_iwt_results = os.path.join( os.path.curdir , 'temp_iwt_results.csv' )
+	os.system( f'Rscript {fname_Rscript} {seed} {niter} {fname_data} {fname_iwt_results}' )
+	p                 = np.loadtxt(fname_iwt_results, delimiter=',')
+	os.remove( fname_iwt_results )
+	return p
+
+def plot_results(p_unadjusted, p_iwt, p_spm, p_snpm, dv_label=None):
+	fig,AX  = plt.subplots( 1, 2, figsize=(8,3) )
+	plt.get_current_fig_manager().window.move(0, 0)
+	ax0,ax1 = AX.flatten()
+	q       = np.linspace(0, 1, p_unadjusted.size)
+	### plot dataset:
+	ax0.plot( q, y0.T, 'k', lw=0.3 )
+	ax0.plot( q, y1.T, 'r', lw=0.3 )
+	ax0.plot( q, y0.mean(axis=0), 'k', lw=5, label='Mean A' )
+	ax0.plot( q, y1.mean(axis=0), 'r', lw=5, label='Mean B' )
+	ax0.legend()
+	if dv_label is not None:
+		ax0.set_ylabel( dv_label )
+	### plot p curves
+	ax1.plot( q, p_unadjusted, 'k', lw=5, label='Unadjusted')
+	ax1.plot( q, p_iwt,  'b', label='IWT')
+	ax1.plot( q, p_spm,  'c', label='SPM')
+	ax1.plot( q, p_snpm, 'm', label='SnPM')
+	ax1.axhline(0.05, color='k', linestyle='--')
+	ax1.set_ylim(-0.05, 1.05)
+	ax1.legend()
+	ax1.set_ylabel( 'Probability' )
+	plt.tight_layout()
+
+
+
+# # (0) Analyze one dataset:
+# dataset_label      = 'Kautz1991a'
+# dirREPO            = unipath.Path( os.path.dirname(__file__) ).parent.parent
+# fname_data         = os.path.join( dirREPO, 'Data', 'ExperimentalData', '%s.csv' %dataset_label)
+# y0,y1              = load_csv(fname_data)
+# p_unadjusted       = calc_p_unadjusted(y0, y1)
+# p_spm              = calc_p_adjusted_spm(y0, y1)
+# p_snpm             = calc_p_adjusted_snpm(y0, y1)
+# p_iwt              = calc_p_adjusted_iwt(fname_data, seed=1, niter=1000)
+# plt.close('all')
+# plot_results(p_unadjusted, p_iwt, p_spm, p_snpm, dv_label='Force (N)')
+# plt.show()
+
+
+
+
+
+
+# (1) Analyze all datasets (and save figures):
+dirREPO           = unipath.Path( os.path.dirname(__file__) ).parent.parent
+dataset_labels    = ['Kautz1991a', 'Kautz1991b', 'Neptune1999', 'Besier2009a', 'Besier2009b', 'Dorn2012']
+dv_labels         = ['Normal pedal force (N)', 'Tangential pedal force (N)', 'Knee flexion (deg)', 'Semimembranosus force (N)', 'Medial gastrocnemius force (N)', 'Anterior ground reaction force (N)']
+for dataset_label, dv_label in zip(dataset_labels, dv_labels):
+	print( f'Processing {dataset_label}...')
+	fname_data    = os.path.join( dirREPO, 'Data', 'ExperimentalData', '%s.csv' %dataset_label)
+	fname_fig     = os.path.join( dirREPO, 'Figures', 'ExperimentalData', '%s.pdf' %dataset_label)
+	y0,y1         = load_csv(fname_data)
+	p_unadjusted  = calc_p_unadjusted(y0, y1)
+	p_spm         = calc_p_adjusted_spm(y0, y1)
+	p_snpm        = calc_p_adjusted_snpm(y0, y1)
+	p_iwt         = calc_p_adjusted_iwt(fname_data, seed=1, niter=1000)
+	plt.close('all')
+	plot_results(p_unadjusted, p_iwt, p_spm, p_snpm, dv_label=dv_label)
+	plt.show()
+	plt.savefig(fname_fig)
+	
+
