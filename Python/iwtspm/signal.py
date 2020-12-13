7527ee8ae73718158412d9cda95f9bcc0236eb37
Todd Pataky <todd@MacBook-Pro-16-todo.local>
2020-12-13 13:10:16 +0900

    Add multipulse signal

--- Python/iwtspm/signal.py
+++ Python/iwtspm/signal.py
@@ -8,6 +8,12 @@ import numpy as np
 # 	fwhm  = w0 + (w1-w0) / (1 + np.exp(-dx*x))
 # 	return fwhm
 
+def multi_pulse(Q=101, q=[50], w=10):
+	a           = np.zeros(Q)
+	for qq in q:
+		a[qq-w:qq+w] = 1
+	return a
+
 def sigmoid_step(Q=101, q0=50, q1=75, x0=0, x1=1):
 	z          = np.zeros(Q)
 	zz         = 6
