fa326af48f68ae85d3aae08b2166fef1792e3a8a
Todd Pataky <todd@h74.50.242.10.103784.vlan.kuins.net>
2020-12-27 07:46:25 +0900

    Update domain figure

--- Python/Simulation/fig_domain.py
+++ Python/Simulation/fig_domain.py
@@ -17,6 +17,7 @@ plt.style.use('bmh')
 mpl.rcParams['xtick.labelsize'] = 'small'
 mpl.rcParams['ytick.labelsize'] = 'small'
 mpl.rcParams['font.sans-serif'] = 'Arial'
+mpl.rcParams["text.usetex"] = False
 
 
 Q     = 101
@@ -24,18 +25,7 @@ amp   = 1
 q0    = 50
 w     = 40
 wfall = 5
-
-# q0       = int( floor( 50 - 0.5 * w ) )
-# q1       = q0 + w
-
-# a        = 0.5 * amp
-# signal1a = power1d.geom.Sigmoid(Q, q0=q0-w, q1=q0, x0=-a, x1=a)
-# signal1b = power1d.geom.Sigmoid(Q, q0=q1, q1=q1+w, x0=a, x1=-a)
-# y1a,y1b  = signal1a.toarray(), signal1b.toarray()
-# y        = y1a + y1b
-
-
-y        = iws.signal.sigmoid_pulse(Q, q0=q0, w=w, wfall=wfall, amp=amp)
+y     = iws.signal.sigmoid_pulse(Q, q0=q0, w=w, wfall=wfall, amp=amp)
 
 
 plt.close('all')
@@ -43,8 +33,8 @@ plt.figure(figsize=(5,3))
 plt.get_current_fig_manager().window.move(0, 0)
 ax = plt.axes()
 q  = np.linspace(0, 1, Q)
-ax.plot(q, np.zeros(Q), color='0.6', lw=7, label=r'$\mu_0$')
-ax.plot(q, y, color='k', lw=3, label=r'$\mu_1$')
+ax.plot(q, np.zeros(Q), color='0.6', lw=7, label=r'$\mu_A$')
+ax.plot(q, y, color='k', lw=3, label=r'$\mu_B$')
 ax.legend(loc='lower right', bbox_to_anchor=(0.98, 0.1))
 
 q00,q01  = 0.01 * np.array([q0 - 0.5*w , q0 + 0.5*w])
@@ -66,11 +56,9 @@ ax.plot([q00,q01], [0.79]*2, color='r', lw=2)
 ax.plot([q00, q10], [0.92]*2, color='g', lw=4)
 ax.plot([q01, q11], [0.92]*2, color='g', lw=4)
 
-
 ax.set_xlabel('Domain position')
 plt.tight_layout()
 plt.show()
-
 
 
 
