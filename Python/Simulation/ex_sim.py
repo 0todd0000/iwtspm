0c282c61565c5a22f6fb54701d552a0c7fdd90e9
Todd Pataky <todd@todds-imac-pro.lan>
2021-05-12 14:05:53 +0900

    Update ex_sim.py

--- Python/Simulation/ex_sim.py
+++ Python/Simulation/ex_sim.py
@@ -1,20 +1,8 @@
 
 '''
-Example single simulation iteration using baseline parameters (see manuscript).
+Example single simulation iteration using baseline parameters.
 
-Baseline parameters:
-
-### Sample parameters:
-- sample size = 20
-- signal amplitude = 1.5
-- signal width = 0.4
-
-### Error parameters:
-- error distribution = Gaussian 
-- SD = 1
-- SD ratio = 1.0  (same standard deviation everywhere)
-- FWHM (smoothness) = 0.2
-- FWHM ratio = 1.0 (same smoothness everywhere)
+Find baseline parameters descriptions in the manuscript (Table 2)
 '''
 
 import os
@@ -29,21 +17,8 @@ import iwtspm as iws
 #(0) Run a single iteration:
 np.random.seed(0)
 wd          = os.path.join( os.path.dirname( __file__ ) , '_wd' )
-### set baseline parameters:
-Q           = 101   # domain size
-J           = 20    # sample size
-sig_amp     = 1     # signal amplitude
-sig_width   = 40    # signal width (100ths)
-sd          = 1     # standard deviation
-sd_ratio    = 1     # standard deviation ratio (D0:D1)
-fwhm        = 20
-fwhm_ratio  = 1
-### construct simulation objects:
-s           = iws.signal.sigmoid_pulse_amps( Q=Q, q0=50, w=sig_width, wfall=5, amp0=(sd*sd_ratio), amp1=sd )
-w           = iws.signal.sigmoid_pulse_amps( Q=Q, q0=50, w=sig_width, wfall=5, amp0=(fwhm*fwhm_ratio), amp1=fwhm)
-gen         = lambda: iws.random.generate_dataset(J, Q, sig_amp=sig_amp, sig_width=sig_width, dist='gauss_matern', distparams=(s,w))
-# y0,y1       = gen()
-sim         = iws.sim.Simulator(wd, gen, suff='')
+params      = iws.sim.SimulationParameters()   # baseline parameters
+sim         = iws.sim.Simulator(wd, params, suffix='')
 sim.clear_wd()
 p0,p1,p2,p3 = sim.run_iteration()
 y0,y1       = sim.get_data()
@@ -53,7 +28,7 @@ fig,AX = plt.subplots( 1, 2, figsize=(8,3) )
 plt.get_current_fig_manager().window.move(0, 0)
 ax0,ax1 = AX.flatten()
 ### plot dataset:
-q   = np.linspace(0, 1, Q)
+q   = np.linspace(0, 1, params.Q)
 ax0.plot(q,  y0.T, 'k', lw=0.5 )
 ax0.plot(q,  y1.T, 'r', lw=0.5 )
 ### plot p curves
